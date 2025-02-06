ed5ed529a35047ad32e9362305556bf7c5daebd3
jayantp@gmail.com <jayantp8767@gmail.com>
2025-02-07 01:26:47 +0530

    Added the Tech Stack Name and Updated Footer

--- src/components/Footere.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer(){
-    return(
-        <div>
-            <p className="m-2 flex justify-end tracking-[0.1em] ">Made with <i className="text-red-500 fa-solid fa-heart m-1"></i> by Jayant.</p>
-        </div>
-    )
-}
